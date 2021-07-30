d9234d20a0377587533b7bfb82415fa7e9cf3f4a
dx3310933lb <dx3310933@hotmail.com>
2021-07-30 17:18:24 +0900

    2021073017時コミット

--- item2/src/main/java/com/dx/controller/TestController.java
+++ item2/src/main/java/com/dx/controller/TestController.java
@@ -1,13 +1,27 @@
 package com.dx.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TestController {
 
     @GetMapping("/hello")
-    public String test(){
+    public String test1(){
         return "hello world springboot!";
+    }
+
+    @PostMapping("/posthello")
+    public String test2(){
+        return "post world springboot!";
+    }
+
+    @DeleteMapping("/deletehello")
+    public String test3(){
+        return "delete world springboot!";
+    }
+
+    @GetMapping("/hello/{userId}/{passwd}")
+    public String test4(@PathVariable String userId, @PathVariable String passwd){
+        return "hello param get!"+ userId + " : " + passwd;
     }
 }
